4e2f3e46df25b6a328011a3af9d16873281aaa18
reconcycle <reconcycle@reconcycle-dev1>
2021-09-14 11:52:07 +0200

    less output during JointTrapVel execution

--- reconcycle_flexbe_states/src/reconcycle_flexbe_states/CallAction_JointTrapVel.py
+++ reconcycle_flexbe_states/src/reconcycle_flexbe_states/CallAction_JointTrapVel.py
@@ -15,8 +15,7 @@ from robot_module_msgs.msg import JointTrapVelAction,JointTrapVelGoal
 class CallJointTrap(EventState):
 
     '''
-    Calls JointTrapVelAction server @ 'joint_trap_vel_action_server' topic
-    [...]
+    Calls JointTrapVelAction server
 
     ># joints_data string/int      The name under which the name is written into MongoDB
 
@@ -31,17 +30,9 @@ class CallJointTrap(EventState):
                                             input_keys = ['joints_data'], 
                                             output_keys = ['joint_values'])
 
-        # replace 'panda_2/joint_trap_vel_action_server' from dummies with 'joint_trap_vel_action_server'
         self._namespace = namespace
-        
         self._topic = self._namespace + '/joint_impedance_controller/move_joint_trap'
-        #self._topic = '/joint_impedance_controller/move_joint_trap'
-
         self._client = ProxyActionClient({ self._topic: JointTrapVelAction})
-        #self._client = ProxyActionClient({self._topic: robot_module_msgs.msg.JointTrapVelAction}) # pass required clients as dict (topic: type)
-        # JointTrapVelAction
-
-		# It may happen that the action client fails to send the action goal.
 
         self.max_vel = max_vel
         self.max_acl = max_acl
@@ -61,57 +52,37 @@ class CallJointTrap(EventState):
             self._client.send_goal(self._topic,goal)
             Logger.loginfo("Goal sent: {}".format(str(self.goal_joint)))
         except Exception as e:
-			# Since a state failure not necessarily causes a behavior failure, it is recommended to only print warnings, not errors.
-			# Using a linebreak before appending the error log enables the operator to collapse details in the GUI.
             Logger.loginfo('Failed to send the goal command:\n{}'.format(str(e)))
             self._error = True
             return 'failed'
 
-        self.timeout = time.time()
+        #self.timeout = time.time()
 
     def execute(self, userdata):
         
-
         try:
-          
             
-            #result = self._client.get_result(self._topic) 
+            #if time.time()-self.timeout > 12:
+            #        break
+                    
             if self._client.has_result(self._topic):
                 result = self._client.get_result(self._topic) 
-                Logger.loginfo("Result {}".format(result))
+                #Logger.loginfo("Result {}".format(result))
                 return 'continue'
-
             else:
-
-
-            #if result == None:
-                
                 feedback = self._client.get_feedback(self._topic)
-                Logger.loginfo("{}".format(feedback))
-                #time.sleep(0.5)
-                # 12 secs timeout
+                #Logger.loginfo("{}".format(feedback))
                 
-                #if time.time()-self.timeout > 12:
-                    #break
-
-                
- 
-            
-
         except Exception as e:
-            
             Logger.loginfo("No result or server is not active!")
             return 'failed'
 
-         
-
     def on_exit(self, userdata):
-
-        Logger.loginfo("Exit state...")
 
         if not self._client.get_result(self._topic):
             self._client.cancel(self._topic)
             Logger.loginfo('Cancelled active action goal.')
+            
         Logger.loginfo('Finished sending goal to JointTrapVelGoal.')
         return 'continue'
         
