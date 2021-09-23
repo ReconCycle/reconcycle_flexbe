6c9d13589ebddf63049ca480efa754a1e42da7bb
matejsgit <matej.stefanic@icloud.com>
2021-09-23 07:44:54 +0000

    Changed topic beeing called

--- reconcycle_flexbe_states/src/reconcycle_flexbe_states/vise_proxy_client.py
+++ reconcycle_flexbe_states/src/reconcycle_flexbe_states/vise_proxy_client.py
@@ -2,7 +2,7 @@
 
 from flexbe_core import EventState, Logger
 from flexbe_core.proxy import ProxyServiceCaller
-from robot_module_msgs.srv import SetList, SetListRequest
+from robot_module_msgs.srv import MoveVise, MoveViseRequest
 import rospy
 
 
@@ -29,47 +29,19 @@ class ChangeViseModelPositionClient(EventState):
         self.condition = condition
         
         # Topic and ServicerCaller init
-        self.topic = "/change_transform_matrix/" + str(self.vise)
-        self.client_proxy = ProxyServiceCaller({self.topic: SetList})
+        self.topic = "/move_vise_tool/" + str(self.vise)
+        self.client_proxy = ProxyServiceCaller({self.topic: MoveVise})
 
     
     def on_enter(self, userdata):
-        Logger.loginfo("Started service client unload controller...")
+        Logger.loginfo("Started service move vise controller...")
 
-        request = SetListRequest()
-
-        if self.condition == True:
-            if self.vise == "vise_plate":
-                request.matrix = [0.05,-0.03,0.019,0,0,0]
-            #elif self.vise == "vise_rotation":
-            #    request.matrix = [-0.125,0.0788,0.1385,180,0,0]
-            elif self.vise == "vise_slider":
-                request.matrix = [-0.215,0.0015,0.024,0,0,0]
-        else:
-            if self.vise == "vise_plate":
-                request.matrix = [0.05,0.14,0.019,0,0,0]
-            #elif self.vise == "vise_rotation":
-            #    request.matrix = [-0.125,0.0788,0.1385,0,0,0]            
-            elif self.vise == "vise_slider":
-                request.matrix = [-0.26,0.0015,0.024,0,0,0]
-            
-        Logger.loginfo("Vise:{} \n Matrix:{}".format(self.vise, request.matrix))
+        request = MoveViseRequest()
+        request.trigger = self.condition
 
         try:
             if self.client_proxy.is_available(self.topic):
-                if self.vise == "vise_rotation":
-                    if self.condition == True:
-                        for i in range(180):
-                            request.matrix = [-0.125,0.0788,0.1385,i,0,0]
-                            success = self.client_proxy.call(self.topic, request)
-                            rospy.sleep(0.001)
-                    else:
-                        for j in range(180,-1,-2):
-                            request.matrix = [-0.125,0.0788,0.1385,j,0,0]
-                            success = self.client_proxy.call(self.topic, request)
-                            rospy.sleep(0.001)                                
-                else:
-                    success = self.client_proxy.call(self.topic, request)
+                success = self.client_proxy.call(self.topic, request)
                 if success.success == True:
                     Logger.loginfo("Successful: {0}".format(success.success))
                     return 'continue'
